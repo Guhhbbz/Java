91930218ed3c8c21c76f549d280695dea9303b9d
Guhhbbz <107234281+Guhhbbz@users.noreply.github.com>
2024-03-11 21:42:30 -0300

    Update Aula1103_04.java

--- aula1103_04/src/aula1103_04/Aula1103_04.java
+++ aula1103_04/src/aula1103_04/Aula1103_04.java
@@ -7,15 +7,13 @@ public class Aula1103_04 {
     public static void main(String[] args) {
         System.out.println("Digite a altura do triangulo: ");
         int a = 0;
-        Scanner altura = new Scanner(System.in);
-        a = altura.nextInt();
+        int b = 0;
+        Scanner ler = new Scanner(System.in);
         
-        System.out.println("Digite o comprimento: ");
-        int c = 0;
-        Scanner comp = new Scanner(System.in);
-        c = comp.nextInt();
-        
-        System.out.println("Area = "+(a*c));
+        a = ler.nextInt();        
+        System.out.println("Digite a base: ");               
+        b = ler.nextInt();        
+        System.out.println("Area = "+(a*b));
     }
     
 }
